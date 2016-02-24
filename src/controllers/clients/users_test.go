e141ff79bba69039daa05fe6b682e14e1024dcb4
Gaston Siffert <gaston.siffert@etixgroup.com>
2016-02-24 10:28:23 +0100

    Add unit test for the client's users

--- src/controllers/clients/users_test.go
+++ src/controllers/clients/users_test.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 
+	"coban/api/src/controllers/common"
 	"coban/api/src/databases"
 	"coban/api/src/utils"
 )
@@ -19,6 +21,7 @@ type usersTestSuite struct {
 func TestUsers(t *testing.T) {
 	utils.InitTest()
 	suite.Run(t, new(usersTestSuite))
+	common.UpdateAccountPassword("user", 1)
 }
 
 func (s *usersTestSuite) Test01GetCurrentUser_ValidUser() {
@@ -41,4 +44,71 @@ func (s *usersTestSuite) Test01GetCurrentUser_ValidUser() {
 	s.NotNil(result)
 	s.Equal(http.StatusOK, result.StatusCode)
 	s.Equal(expected, string(body))
+}
+
+func (s *usersTestSuite) Test02GetCurrentUser_InvalidUser() {
+	token, _ := utils.GenerateToken(2, databases.OfficeScope)
+
+	url := fmt.Sprintf("%s/clients/users", utils.Address)
+	request, _ := http.NewRequest("GET", url, nil)
+	request.Header.Set("Authorization", "Bearer" + " " + token)
+
+	client := &http.Client{}
+	result, _ := client.Do(request)
+	defer result.Body.Close()
+
+	s.Equal(http.StatusUnauthorized, result.StatusCode)
+}
+
+func (s *usersTestSuite) Test03UpdatePassword_ValidUserValidPassword() {
+	data := `{"old-password":"user","password-1":"test","password-2":"test"}`
+	token, _ := utils.GenerateToken(1, databases.ClientScope)
+
+	url := fmt.Sprintf("%s/clients/users", utils.Address)
+	request, _ := http.NewRequest("PUT", url, strings.NewReader(data))
+	request.Header.Set("Authorization", "Bearer" + " " + token)
+
+	client := &http.Client{}
+	result, _ := client.Do(request)
+	defer result.Body.Close()
+
+	s.Equal(http.StatusOK, result.StatusCode)
+}
+
+func (s *usersTestSuite) Test04UpdatePassword_ValidUserInvalidPassword() {
+	token, _ := utils.GenerateToken(1, databases.ClientScope)
+
+	data := `{"old-password":"user","password-1":"test","password-2":"test"}`
+	url := fmt.Sprintf("%s/clients/users", utils.Address)
+	request, _ := http.NewRequest("PUT", url, strings.NewReader(data))
+	request.Header.Set("Authorization", "Bearer" + " " + token)
+
+	client := &http.Client{}
+	result, _ := client.Do(request)
+	defer result.Body.Close()
+	s.Equal(http.StatusBadRequest, result.StatusCode)
+
+	data = `{"old-password":"test","password-1":"pass","password-2":"pass2"}`
+	request, _ = http.NewRequest("PUT", url, strings.NewReader(data))
+	request.Header.Set("Authorization", "Bearer" + " " + token)
+
+	client = &http.Client{}
+	result, _ = client.Do(request)
+	defer result.Body.Close()
+	s.Equal(http.StatusBadRequest, result.StatusCode)
+}
+
+func (s *usersTestSuite) Test05UpdatePassword_InvalidUserValidPassword() {
+	data := `{"old-password":"user","password-1":"test","password-2":"test"}`
+	token, _ := utils.GenerateToken(2, databases.OfficeScope)
+
+	url := fmt.Sprintf("%s/clients/users", utils.Address)
+	request, _ := http.NewRequest("PUT", url, strings.NewReader(data))
+	request.Header.Set("Authorization", "Bearer" + " " + token)
+
+	client := &http.Client{}
+	result, _ := client.Do(request)
+	defer result.Body.Close()
+
+	s.Equal(http.StatusUnauthorized, result.StatusCode)
 }
